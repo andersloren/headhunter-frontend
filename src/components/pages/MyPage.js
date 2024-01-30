defef150922769c6d6ea9f62e1daa946e3d3b837
Liebniz <anders.leif.loren@liebniz.com>
2024-01-30 18:48:15 +0100

    found jwt-decoder

--- src/components/pages/MyPage.js
+++ src/components/pages/MyPage.js
@@ -1,3 +1,15 @@
-export default function MyPage() {
-  return <div>My Page</div>;
+import { jwtDecode } from "jwt-decode";
+
+export default function MyPage({ token }) {
+  function handleButton() {
+    const user = jwtDecode(token);
+    console.log(user);
+  }
+
+  return (
+    <div>
+      My Page
+      <button onClick={handleButton}>Decode Token</button>
+    </div>
+  );
 }
